d083f52e77d0a04826ac983a98af4963f34279bb
Hannu Korvala <hannu.s.korvala@gmail.com>
2023-08-27 13:36:03 +0300

    Even more flaws!

--- brokensite/views.py
+++ brokensite/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from django.http import JsonResponse
 from .models import Note
 from django.utils import timezone
 from django.db import connection
 import json
+from django.views.decorators.csrf import csrf_exempt
 
 
 @login_required
@@ -23,26 +25,24 @@ def index(request):
     return render(request, "brokensite/index.html", context)
 
 
-@login_required
+# Flaw: csrf exempted
+# this enables an attack where attacker forces authenticated user to submit a request
+# to this web application
+@csrf_exempt
+@login_required()
 def delete(request):
     if request.method == "POST":
         post_data = json.loads(request.body.decode("utf-8"))
         note_id = post_data.get('note_id')
 
         if note_id is not None:
-            # A01:2021 – Broken Access Control
-            # Access control enforces policy such that users cannot act outside of their intended permissions.
-            # Logged in users can delete each others notes if they know the note id
-            # A simple fix is to add the following line:
-            # if request.user == note.user:
-
             # A03:2021 – Injection
             # This is unsafe and gives in the possibility for an injection attack
             # should be using the following instead:
             # note = get_object_or_404(Note, pk=note_id)
             # note.delete()
             with connection.cursor() as cursor:
-                query = "DELETE FROM brokensite_note WHERE id=" + str(note_id) + ""
+                query = "DELETE FROM brokensite_note WHERE user_id=" + str(request.user.id) + " AND id=" + str(note_id)
                 cursor.executescript(query)
 
     notes = Note.objects.filter(user=request.user).order_by('-date_published')
@@ -50,11 +50,15 @@ def delete(request):
     return render(request, "brokensite/index.html", context)
 
 
+# A01:2021 – Broken Access Control icon
+# @login_required missing, user id is retrieved from request
 def search(request):
-    keyword = request.GET.get('textToSearch', None)
+    keyword = request.GET.get('keyword', None)
+    # should be request.user.id
+    userid = request.GET.get('userid', None)
 
     if keyword is not None:
-        notes = Note.objects.filter(note_text__contains=keyword, user_id=request.user.id)
+        notes = Note.objects.filter(note_text__contains=keyword, user_id=userid)
         context = {"notes": notes}
     else:
         context = {"notes": None}
